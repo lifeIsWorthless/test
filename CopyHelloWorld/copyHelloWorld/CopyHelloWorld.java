8d91df622ec2af3180816b8930b3d91c8f7360a9
Richard Howard <RichardHoward@RichardHoward.ca>
2016-07-26 09:55:14 -0400

    4

--- CopyHelloWorld/copyHelloWorld/CopyHelloWorld.java
+++ CopyHelloWorld/copyHelloWorld/CopyHelloWorld.java
@@ -2,6 +2,6 @@ package copyHelloWorld;
 
 public class CopyHelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World! 3");
+		System.out.println("Hello, World! 4");
 	}
 }
